8c5e2e50ce757586ecd8749efe9077b8a785ab20
Salvador Guido Juarez <salvadorgjuarez@gmail.com>
2018-11-05 15:02:41 -0600

    Output functions - led movement

---
+++ src/output_functions.h
@@ -0,0 +1,17 @@
+/*
+ * output_functions.h
+ *
+ *  Created on: Nov 5, 2018
+ *      Author: maver
+ */
+
+#ifndef OUTPUT_FUNCTIONS_H_
+#define OUTPUT_FUNCTIONS_H_
+
+
+void led_output(int ui32_value);
+void turn_off (void);
+void go_up (void);
+void go_down (void);
+
+#endif /* OUTPUT_FUNCTIONS_H_ */
